9d0076f20d9e012212f9b2c6473f2c0dc6c55e28
Moh. Walid Arkham Sani <mohwalidas@outlook.com>
2023-07-21 19:57:18 +0800

    feat: add login page

---
+++ src/routes/(sub-home)/profil/+page.svelte
@@ -0,0 +1,139 @@
+<div class="relative flex flex-col w-full min-h-screen bg-white">
+    <div class="flex items-center justify-between py-1 space-x-2.5">
+        <a href="/" class="px-3 py-4 active:opacity-80">
+            <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                aria-hidden="true"
+                class="h-auto w-8"
+            >
+                <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M15.75 19.5L8.25 12l7.5-7.5"
+                />
+            </svg>
+        </a>
+        <div class="flex-1 text-lg font-medium text-center capitalize truncate">
+            Profil Saya
+        </div>
+        <div class="px-3 py-4">
+            <div class="w-7">​</div>
+        </div>
+    </div>
+    <main>
+        <div class="py-4">
+            <div class="flex items-center w-full px-5 space-x-5">
+                <img
+                    alt="user"
+                    class="rounded-3xl bg-gray-100 w-20"
+                    src="https://ui-avatars.com/api/?name=Jiilaan+Aanisah&amp;color=7F9CF5&amp;background=EBF4FF"
+                />
+                <div class="flex flex-col w-full text-left">
+                    <p class="text-xl font-medium">Jiilaan Aanisah</p>
+                    <p class="text-gray-500 text-sm">KPPN Marisa</p>
+                </div>
+            </div>
+        </div>
+        <hr class="border-4 border-gray-100" />
+        <div class="py-4">
+            <ul>
+                <li>
+                    <button
+                        type="button"
+                        class="flex items-center justify-between w-full px-5 py-4 space-x-4 group active:bg-gray-100"
+                    >
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            stroke-width="1.5"
+                            stroke="currentColor"
+                            aria-hidden="true"
+                            class="w-5"
+                        >
+                            <path
+                                stroke-linecap="round"
+                                stroke-linejoin="round"
+                                d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z"
+                            />
+                        </svg>
+                        <div
+                            class="flex-1 min-w-0 text-base text-left text-gray-700 group-active:text-gray-900"
+                        >
+                            Ubah Profil
+                        </div>
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            stroke-width="1.5"
+                            stroke="currentColor"
+                            aria-hidden="true"
+                            class="w-5"
+                        >
+                            <path
+                                stroke-linecap="round"
+                                stroke-linejoin="round"
+                                d="M8.25 4.5l7.5 7.5-7.5 7.5"
+                            />
+                        </svg>
+                    </button>
+                </li>
+                <li>
+                    <button
+                        type="button"
+                        class="flex items-center justify-between w-full px-5 py-4 space-x-4 group active:bg-gray-100"
+                    >
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            stroke-width="1.5"
+                            stroke="currentColor"
+                            aria-hidden="true"
+                            class="w-5"
+                        >
+                            <path
+                                stroke-linecap="round"
+                                stroke-linejoin="round"
+                                d="M11.25 11.25l.041-.02a.75.75 0 011.063.852l-.708 2.836a.75.75 0 001.063.853l.041-.021M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9-3.75h.008v.008H12V8.25z"
+                            />
+                        </svg>
+                        <div
+                            class="flex-1 min-w-0 text-base text-left text-gray-700 group-active:text-gray-900"
+                        >
+                            Tentang Caca Marisa Mobile
+                        </div>
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            stroke-width="1.5"
+                            stroke="currentColor"
+                            aria-hidden="true"
+                            class="w-5"
+                        >
+                            <path
+                                stroke-linecap="round"
+                                stroke-linejoin="round"
+                                d="M8.25 4.5l7.5 7.5-7.5 7.5"
+                            />
+                        </svg>
+                    </button>
+                </li>
+            </ul>
+        </div>
+        <div class="px-5 mt-20">
+            <a
+                href="/login"
+                class="flex w-full items-center justify-center px-8 py-5 rounded-3xl active:bg-opacity-80 disabled:bg-opacity-40 disabled:text-opacity-50 bg-blue-500 text-white"
+            >
+                Logout
+            </a>
+        </div>
+    </main>
+</div>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,8 @@
 <script>
     import '@fontsource/poppins/400.css'
+    import '@fontsource/poppins/500.css'
     import '@fontsource/poppins/600.css'
+    import '@fontsource/poppins/700.css'
     import '@fontsource/poppins/800.css'
     import '$lib/assets/css/app.css'
 </script>
